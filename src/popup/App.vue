3b2f379c4fdef658a91b20ef3eeb14e0396bd7c7
wormwlrm <wormwlrm@naver.com>
2019-05-12 17:57:01 +0900

    First Commit

---
+++ src/popup/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Hello world!2222333</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
